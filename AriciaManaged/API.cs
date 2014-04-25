a7034de0f750b82ed88b795037659e0630dc8000
jsnmc <jsnmcdev@gmail.com>
2014-04-25 01:12:36 -0400

    Start to create the C# api layer modeled after Sikuli.

---
+++ AriciaManaged/API.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Class Region
+ * Region is a rectangular area on a screen, which is defined by
+ * its upper left corner (x, y) as a distance relative to the upper left corner of the screen (0, 0) and
+ * its dimension (w, h) as its width and height.
+ * x, y, w, h are integer numbers counting a distance in pixels.
+ * 
+ * find(PS)
+ * Parameters:	PS – a Pattern object or a string (path to an image file or just plain text)
+ * Returns:	a Match object that contains the best match or fails if not found
+ * 
+ * findAll(PS)
+ * Parameters:	PS – a Pattern object or a string (path to an image file or just plain text)
+ * Returns:	one or more Match objects as an iterator object or fails if not found
+ * 
+ * wait([PS][, seconds])
+ * Parameters:	
+ * PS – a Pattern object or a string (path to an image file or just plain text)
+ * seconds – a number, which can have a fraction, as maximum waiting time in seconds. The internal granularity is milliseconds. If not specified, the auto wait timeout value set by Region.setAutoWaitTimeout() is used. Use the constant FOREVER to wait for an infinite time.
+ * Returns:a Match object that contains the best match or fails if not found
+ * 
+ * waitVanish(PS[, seconds])
+ * Wait until the give pattern PS in the region vanishes.
+ * Parameters:	
+ * PS – a Pattern object or a string (path to an image file or just plain text)
+ * seconds – a number, which can have a fraction, as maximum waiting time in seconds. The internal granularity is milliseconds. If not specified, the auto wait timeout value set by Region.setAutoWaitTimeout() is used. Use the constant FOREVER to wait for an infinite time.
+ * Returns:	True if the pattern vanishes within the specified waiting time, or False if the pattern stays visible after the waiting time has elapsed.
+ * 
+ * exists(PS[, seconds])
+ * Check whether the give pattern is visible on the screen.
+ * Parameters:	
+ * PS – a Pattern object or a string (path to an image file or just plain text)
+ * seconds – a number, which can have a fraction, as maximum waiting time in seconds. The internal granularity is milliseconds. If not specified, the auto wait timeout value set by Region.setAutoWaitTimeout() is used. Use the constant FOREVER to wait for an infinite time.
+ * Returns:	a Match object that contains the best match. None is returned, if nothing is found within the specified waiting time
+ * Does exactly the same as Region.wait(), but no exception is raised in case of FindFailed. 
+ * So it can be used to symplify scripting in case that you only want to know wether something is there or not to decide how to proceed in your workflow. 
+ * 
+ * Class Match: 
+ *
+ * 
+ * 
+ */
---
+++ AriciaManaged/IRegion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aricia
+{
+    interface IRegion
+    {
+        IMatch find(string targetImagePath);
+        IEnumerable<IMatch> findAll(string targetImagePath);
+        IMatch wait(string targetImagePath, int timeToWait);
+        bool waitVanish(string targetImagePath, int timeToWait);
+        IMatch exists(string targetImagePath, int timeToWait);
+    }
+}
